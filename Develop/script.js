a1e9b9f9acd3cb0111372e3fa9c5e4e6b6d3bd0d
Martha Potts <marthajpotts7@gmail.com>
2021-02-28 00:24:24 -0500

    adding localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,32 @@
-//has link to bootstrapcdn
-//has link to fontawesome
-//has link to googlefonts
-//has .css
+//date at top
+var today = moment();
+$('#currentDay').text(today.format('dddd, MMMM Do'));
 
+//localStorage: get/stringify, set/parse
+var events = {
+    "9": [], 
+    "10": [],
+    "11": [],
+    "12": [],
+    "13": [],
+    "14": [],
+    "15": [],
+    "16": [],
+    "17": []
+};
 
-//body 
-//<header class="jumbotron"
-
-//<h1 class="display-3"
-//<p class="lead"
-//<p id="currentDay" class="lead"
-
-
-//<div class="container" where the Timeblocks go
-
-//has src= cdn.js...jQuery & src= cdn.moment.js & script.js
-
-//CSS textarea; .jumbotron; .description; .time-block; .row; .hour
-//.past; .present; .future; 
-// .saveBtn  (style & i:hover)
-
-//grid/layout/jumbotron: Bootstrap CDN
-//widget (sortable, datepicker): jQueryUI
-//time elements: moment.js
+var setEvents = function(){
+    localStorage.setItem(events, JSON.stringify(events));
+}
+var getEvents = function(){
+   var currentEvents = localStorage.getItems(events, JSON.parse(events));
+   if(currentEvents){
+       events = currentEvents
+       //create events/tasks function where fields do not clear
+       $.each(events, function(hour, task){
+           var hourDiv = $('#' + hour);
+           createEvent(task, hourDiv);
+       })
+       }
+   }
+   
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # workdayScheduler
+
+## Objectives
+
+CDN = Content Delivery/Distribution Network = geographically distrubited proxy servers and their data centers
+-CDNs: CSS Frameworks, JS Libraries, custom fonts
+
+-- jQuery: DOM traversal/manipulation: implement interactivity using event handlers; resolve issues r/t **dynamically generated HTML** using jQuery
+
+--Bootstrap: Grid & Layout/HTML: integrate Bootstrap Components in HTML to build UI
+
+---add custom styling 
+
+--jQuery UI widgets
+
+--Moment.js
+
+## WHAT
+
+Workday Scheduler, std work hours (9-5)
+
+## WHEN
+
+@Open: displays current day/time at top, 
+@scroll: time-blocks of hours 9a - 5p
+@view for that day: color coded past, present, future
+@click: can 'enter an event'
+@click on time-block hour: text is saved in localStorage
+@refresh: saved events persist
+
+## WHERE
+
+URL to GitHub Repo
+
+URL to deployment/GH pages
