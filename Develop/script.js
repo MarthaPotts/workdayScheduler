f95032b053ea2d13819f418d12808542c85ce12b
Martha Potts <marthajpotts7@gmail.com>
2021-02-27 20:53:52 -0500

    sending initial frame

---
+++ Develop/script.js
@@ -0,0 +1,25 @@
+//has link to bootstrapcdn
+//has link to fontawesome
+//has link to googlefonts
+//has .css
+
+
+//body 
+//<header class="jumbotron"
+
+//<h1 class="display-3"
+//<p class="lead"
+//<p id="currentDay" class="lead"
+
+
+//<div class="container" where the Timeblocks go
+
+//has src= cdn.js...jQuery & src= cdn.moment.js & script.js
+
+//CSS textarea; .jumbotron; .description; .time-block; .row; .hour
+//.past; .present; .future; 
+// .saveBtn  (style & i:hover)
+
+//grid/layout/jumbotron: Bootstrap CDN
+//widget (sortable, datepicker): jQueryUI
+//time elements: moment.js
