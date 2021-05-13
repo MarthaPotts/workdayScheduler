7944910c253eece602e5193f5a9304cbacb7374e
Martha Potts <marthajpotts7@gmail.com>
2021-05-12 20:52:48 -0400

    updated css and js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         let currentHour = moment().hours(); 
         //iterate over time blocks 
         $(".time-block").each(function(){
-            let blockHour = parseInt($(this).attr("id").split("-")[1]); 
+            let blockHour = parseInt($(this).attr("id")); 
             if (blockHour < currentHour) {
                 $(this).addClass("past"); 
             } else if (blockHour === currentHour) {
@@ -31,15 +31,15 @@ $(document).ready(function() {
 
     let interval = setInterval(hourUpdater, 15000); 
 
-    $("#hour-9 .description").val(localStorage.getItem("hour-9")); 
-    $("#hour-10 .description").val(localStorage.getItem("hour-10")); 
-    $("#hour11 .description").val(localStorage.getItem("hour-11")); 
-    $("#hour-12 .description").val(localStorage.getItem("hour-12")); 
-    $("#hour-13 .description").val(localStorage.getItem("hour-13")); 
-    $("#hour-14 .description").val(localStorage.getItem("hour-14")); 
-    $("#hour-15 .description").val(localStorage.getItem("hour-15")); 
-    $("#hour-16 .description").val(localStorage.getItem("hour-16")); 
-    $("#hour-17 .description").val(localStorage.getItem("hour-17")); 
+    $("#9 .description").val(localStorage.getItem("9")); 
+    $("#10 .description").val(localStorage.getItem("10")); 
+    $("#11 .description").val(localStorage.getItem("11")); 
+    $("#12 .description").val(localStorage.getItem("12")); 
+    $("#13 .description").val(localStorage.getItem("13")); 
+    $("#14 .description").val(localStorage.getItem("14")); 
+    $("#15 .description").val(localStorage.getItem("15")); 
+    $("#16 .description").val(localStorage.getItem("16")); 
+    $("#17 .description").val(localStorage.getItem("17")); 
     
     $("#currentDay").text(moment().format("dddd, MMMM Do")); 
 }); 
